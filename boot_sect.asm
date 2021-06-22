be1f38524230ab33b967b0d982b8bf747271277c
BuildTools <unconfigured@null.spigotmc.org>
2021-06-22 17:28:19 +0100

    Added Print function to print "Hello" to screen

--- boot_sect.asm
+++ boot_sect.asm
@@ -1,7 +1,29 @@
-; Simple assembly boot sector, makes the sector loop forever
+; Simple assembly boot sector, That prints a message to the screen using a routine
 
-loop:                   ; Define a label, used to jump back to
-    jmp loop            ; Instruction used to jump to a label somewhere in the code
+
+; The way the a- registers work here (ax, ah, al) are as follows
+; mov ax, 0    -> 0x0000
+; mov ah, 0x56 -> 0x5600
+; mov al, 0x23 -> 0x5623
+; mov ah, 0x16 -> 0x1623
+; mov ax, 0    -> 0.0000
+; These 3 registers are all linked, ax referring to the whole word of data
+; ah reffers to only the first byte, and al reffers to only the second
+
+mov ah, 0x0e            ; int 10/ah = 0eh -> scrolling teletype BIOS routine used for writing to the screen
+
+mov al, 'H'             ; Put the ascii character 'H' in the AL register (0x48)
+int 0x10                ; int 10/ah = 0eh -> scrolling teletype BIOS routine used for writing to the screen
+mov al, 'e'
+int 0x10
+mov al, 'l'
+int 0x10
+mov al, 'l'
+int 0x10
+mov al, 'o'
+int 0x10
+
+jmp $                   ; This is used to jump to the current address in memory (Which in this case loops forever)
 
 times 510-($-$$) db 0   ; When the code is compiled, the boot sector must be a multiple of 512 butes, the final 2 byutes needing to be 0xaa55
                         ; This line tells the program to pad the file up to 510 bytes, we add the final 2 manually.
